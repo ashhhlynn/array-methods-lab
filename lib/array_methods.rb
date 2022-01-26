35099a17dfb3e32d29e5408dcebf4061f200aa16
ashhhlynn <ashleylynnwelch@gmail.com>
2022-01-25 19:31:33 -0600

    commit tree c0e2351acaedf94d85a326121597a9c1ed99bbe7

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,28 @@
 def using_include(array, element)
-
+    array.include?(element)
 end
 
 def using_sort(array)
-
+array = ["I", "bananas", "wow"]
+array.sort
 end
 
 def using_reverse(array)
-
+    array = ["wow", "bananas", "arrays!"]
+    array.reverse
 end
 
 def using_first(array)
-
+    array = ["wow", "bananas", "grapes"]
+    array.first
 end
 
 def using_last(array)
-
+    array = ["apples", "bananas", "arrays!"]
+    array.last
 end
 
 def using_size(array)
-
+    array = ["apples", "bananas", "grapes", "anything", "else", "ok"]
+    array.size
 end
